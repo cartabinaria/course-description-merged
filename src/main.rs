e9b232fda18688eda88f0f1b13b609fcdf65e064
Stefano Volpe <foxy@teapot.ovh>
2026-01-02 06:18:04 +0100

    style: functional rewrite

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,14 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use degrees::degrees;
-use log::error;
-use std::{fmt::Write, fs};
+mod degrees;
+mod logging;
+mod writer;
 
-pub mod degrees;
+use logging::setup;
+use writer::write_all;
 
 fn main() {
-    let env = env_logger::Env::default().default_filter_or("info");
-    env_logger::Builder::from_env(env).init();
-    if let Err(e) = color_eyre::install() {
-        error!("Eyre setup: {e}");
-        return;
-    };
-    let output_dir = std::path::Path::new("output");
-    if !output_dir.exists() {
-        if let Err(e) = fs::create_dir(output_dir) {
-            error!("Output dir creation: {e}");
-            return;
-        };
-    }
-    let mut index = "= Unified Course Descriptions for Some UNIBO Degrees\n\nhttps://cartabinaria.students.cs.unibo.it/en/wiki/web-scraper/course-description-merged/[Documentation]\n\n".to_owned();
-    if let Some(deg) = degrees() {
-        for d in deg {
-            degrees::analyze_degree(&d, output_dir);
-            if let Err(e) = writeln!(
-                index,
-                "== {}\n\nxref:degree-{}.adoc[web] | link:degree-{}.pdf[PDF] | link:degree-{}.adoc[Asciidoc]\n\n",
-                d.name, d.slug, d.slug, d.slug
-            ) {
-                error!("Could not append {}: {}", d.name, e);
-            };
-        }
-    } else {
-        error!("Could not load degrees");
-        return;
-    }
-    if let Err(e) = fs::write(output_dir.join("index.adoc"), index) {
-        error!("Could not write index: {e}")
-    };
+    setup();
+    write_all();
 }
